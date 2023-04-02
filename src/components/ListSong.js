872c8fd8a83f87a099b87203ea44b8b931c7902a
doandung <doantrungdung2001@gmail.com>
2023-04-02 14:06:36 +0700

    import data

--- src/components/ListSong.js
+++ src/components/ListSong.js
@@ -5,7 +5,25 @@ import React from 'react';
 function ListSong() {
     return (
         <div className='col-span-2'>
-            s
+            <table className='table-auto w-full'>
+                <thead className='text-white h-12'>
+                    <tr>
+                        <th className='w-[10%]'>#</th>
+                        <th className='text-left'>Titile</th>
+                        <th className='w-[10%]'>Author</th>
+                        <th className='w-[10%]'><i className='fa fa-download'>d</i></th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td className='text-center'>1</td>
+                        <td>1</td>
+                        <td className='text-center'>1</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 }
